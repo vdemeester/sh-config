8605f4b70e080f0fdb405669ddde741ac0445a92
Vincent Demeester <vincent.demeester@nextep.net>
2011-10-05 16:34:46 +0200

    Add default prompt (bash) and aliases

---
+++ rc/01-prompt.sh
@@ -0,0 +1,42 @@
+#
+# The prompt should be the following :
+#
+#     $user at $host in $path on $scm:$branch [$revision]
+#     ?+ $
+#
+COLOR_DEFAULT="[00m"
+COLOR_WHITE="[37m"
+COLOR_GREEN="[32m"
+COLOR_RED="[31m"
+COLOR_ORANGE="[1;31m"
+COLOR_BLUE="[34m"
+COLOR_YELLOW="[33m"
+COLOR_MAGENTA="[35m"
+COLOR_VIOLET="[1;35m"
+
+if test `id -u` == 0; then
+    USER_COLOR=$COLOR_RED
+else
+    USER_COLOR=$COLOR_GREEN
+fi
+
+VCPROMPT=`which vcprompt`
+lastcommanedfailed_prompt() {
+    code=$?
+    if test $code != 0; then
+        echo -n $'\033[31m'
+        echo -n "exited ${code} "
+        echo -n $'\033[00m'
+    fi
+}
+
+vde_vcprompt() {
+    ${VCPROMPT} -f $'on \033[35m%n:%b \033[1;35m[%r]\n\033[32m%u%m\033[00m'
+}
+
+export VDE_PROMPT='\e${USER_COLOR}\u \e${COLOR_DEFAULT}at \e${COLOR_BLUE}\h \
+\e${COLOR_DEFAULT}in \e${COLOR_YELLOW}\w\e${COLOR_DEFAULT} \
+`lastcommanedfailed_prompt`\
+`vde_vcprompt`\
+\e${COLOR_DEFAULT}$ '
+export PS1="${VDE_PROMPT}"
---
+++ rc/02-aliases.sh
@@ -0,0 +1,32 @@
+UNAME=$(uname)
+alias oissh="TERM=xterm luit -x -encoding ISO-8859-15 ssh"
+if test "${UNAME}" == "Darwin"; then
+    # -p is for handshake. This is needed on FreeBSD and Darwin
+    # to get it work right.
+    alias oissh="TERM=xterm luit -x -p -encoding ISO-8859-15 ssh"
+    alias ls="ls -G"
+    alias ll="ls -"
+elif test "${UNAME}" == "Linux"; then
+    alias ls="ls --color=always"
+    alias ll="ls -l --group-directories-first"
+fi
+alias la="ls -a" # Show hidden files
+alias lx="ls -lXB" # Sort by extension
+alias lk="ls -lSr" # Sort by size (biggest last)
+alias lc="ls -ltcr"         # sort by and show change time, most recent last
+alias lu="ls -ltur"         # sort by and show access time, most recent last
+alias lt="ls -ltr"          # sort by date, most recent last
+alias lm="ls -al |more"     # pipe through 'more'
+alias lr="ls -lR"           # recursive ls
+alias lt="ls -lt"
+
+alias tree="tree -C"
+# processes
+alias psa='ps auxwww'
+# pipe into grep to find a proc
+alias psg='psa | grep'
+
+alias lsofp='sudo lsof -p'
+
+# which show all versions of an executable
+alias whicha="which -a"
