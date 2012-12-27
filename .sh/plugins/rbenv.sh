752f4358317af6c5e983617dc4f66530684fdbfe
Vincent Demeester <vincent+git@demeester.fr>
2012-12-27 11:01:30 +0100

    Clean old stuff

--- .sh/plugins/rbenv.sh
+++
@@ -1,9 +0,0 @@
-# rbenv plugin
-# Start rbenv stuff if present
-if test -d $HOME/.rbenv; then
-    command -v rbenv >/dev/null || {
-        echo "plugins/rbenv: rbenv not in PATH, you are missing something… I guess"
-        return
-    }
-    eval "$(rbenv init -)"
-fi
